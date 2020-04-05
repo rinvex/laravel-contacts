8dc7af52641e5bf1ea7fee5cb15b67e8c9613a7a
Abdelrahman Omran <me@omranic.com>
2020-04-05 05:57:18 +0000

    Fix namespace issue

--- src/Providers/ContactsServiceProvider.php
+++ src/Providers/ContactsServiceProvider.php
@@ -48,8 +48,8 @@ class ContactsServiceProvider extends ServiceProvider
     public function boot()
     {
         // Publish Resources
-        $this->publishesConfig('rinvex/contacts');
-        $this->publishesMigrations('rinvex/contacts');
-        ! $this->autoloadMigrations('rinvex/contacts') || $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+        $this->publishesConfig('rinvex/laravel-contacts');
+        $this->publishesMigrations('rinvex/laravel-contacts');
+        ! $this->autoloadMigrations('rinvex/laravel-contacts') || $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
     }
 }
